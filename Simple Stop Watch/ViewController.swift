cb8277abdcc2c8991098cc6604bfff0657b5f707
Jonghyun Kim <jhkim@dit.ac.kr>
2018-04-12 10:47:52 +0900

    add start, stop, reset button add Button State

--- Simple Stop Watch/ViewController.swift
+++ Simple Stop Watch/ViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
     var count = 0
     var myTimer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in
-            self.updateTime()
-        })
     }
 
     func updateTime() {
@@ -27,6 +27,31 @@ class ViewController: UIViewController {
         let sec = (count - (min * 60 * 100)) / 100
         let msec = count - (min * 60 * 100) - (sec * 100)
         timeLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
+    }
+    
+    @IBAction func startButton(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in
+            self.updateTime()
+        })
+        changeButoonState(start: false, stop: true, reset: true)
+    }
+    
+    @IBAction func stopButton(_ sender: Any) {
+        myTimer.invalidate()
+        changeButoonState(start: true, stop: false, reset: true)
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        myTimer.invalidate()
+        timeLabel.text = "00:00:00"
+        count = 0
+        changeButoonState(start: true, stop: true, reset: false)
+    }
+    
+    func changeButoonState(start: Bool, stop: Bool, reset: Bool) {
+        startButton.isEnabled = start
+        stopButton.isEnabled = stop
+        resetButton.isEnabled = reset
     }
 
 }
